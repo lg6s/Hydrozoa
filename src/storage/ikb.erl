e2bb77ddfe4d43b6b66cfe4d2a793120ad872c21
mxar <max.rosssner@gmail.com>
2017-06-14 21:37:08 +0200

    temp

--- src/storage/ikb.erl
+++ src/storage/ikb.erl
@@ -1,16 +1,27 @@
 -module(ikb).
 -export([defj/1, mkj/5, ssj/2, rmj/1, gnt/0, rmt/1, scs/0, lhs/0]).
 
-query(Q) -> 
-	{ok, P} = q:connect(<<"127.0.0.1">>, 1843)
-	R = q:execute(P, <<Q>>)
-	q:close(P)
-	R.
+bld_kdb() -> ets:new(kdbcons), add_kdb(lkdbcon, "127.0.0.1", 1843).
+
+bld_kdb(K, H, P) > 
+	case q:connect(<<H>>, P) of
+		{ok, Pid} -> ets:insert(kdbcons, {K, Pid});
+		{error, E} -> erlang:error(E);
+		_ -> erlang:error("unknown error");
+	end.
+
+del_kdb(K) -> q:close(ets:lookup(kdbcons, K)), ets:delete(kdbcons, K).
+
+del_kdb() -> 
+	case ets:first(kdbcons) of 
+		'$end_of_table' -> ets:delete(kdbcons);
+		K -> q:close(ets:lookup(kdbcons, K)),
+			ets:delete(kdbcons, K),
+			del_kdbconn()
+	end.
 
 
-defj(J) -> 
-
-
+defj(J) -> erlang:error("not implemented").
 mkj(P,D,O,L,J) -> erlang:error("not implemented").
 ssj(J,S) -> erlang:error("not implemented").
 rmj(J) -> erlang:error("not implemented").
