9a1c7b963ff118092f0f11cdd50902d13eb58a33
mxar <max.rosssner@gmail.com>
2017-06-14 07:07:30 +0200

    ikb changes

--- src/storage/ikb.erl
+++ src/storage/ikb.erl
@@ -1,7 +1,16 @@
 -module(ikb).
 -export([defj/1, mkj/5, ssj/2, rmj/1, gnt/0, rmt/1, scs/0, lhs/0]).
 
-defj(J) -> erlang:error("not implemented").
+query(Q) -> 
+	{ok, P} = q:connect(<<"127.0.0.1">>, 1843)
+	R = q:execute(P, <<Q>>)
+	q:close(P)
+	R.
+
+
+defj(J) -> 
+
+
 mkj(P,D,O,L,J) -> erlang:error("not implemented").
 ssj(J,S) -> erlang:error("not implemented").
 rmj(J) -> erlang:error("not implemented").
